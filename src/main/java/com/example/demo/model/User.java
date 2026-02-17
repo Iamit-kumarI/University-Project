b3f3a79826902770f92a925666d6a62f40695481
Amit <amitkumaramitkumar15351@gmail.com>
2026-02-17 13:31:13 +0530

    fixed connection with mongodb atlas and creted user type document

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String Id;
+
+    private String email;
+    private String name;
+    private String password;
+}
